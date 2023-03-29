6788389470bfbadb02dae77a200bdeee5ddba058
Reyhan Nathanael <reyhan.nathanael@yahoo.com>
2023-03-29 15:25:10 +0700

    Live Editor fix

--- src/components/TableFeatures/TableFeatures.tsx
+++ src/components/TableFeatures/TableFeatures.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import mermaid from 'mermaid';
 
 interface Props {
@@ -8,11 +8,20 @@ interface Props {
 const SchemaEditor: React.FC<Props> = ({ features }) => {
 	const [inputValue, setInputValue] = useState<string>(features);
 	const [diagramHTML, setDiagramHTML] = useState<string>('');
+  const [cursor, setCursor] = useState<number>(0);
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    const input = ref.current;
+    if (input) input.setSelectionRange(cursor, cursor);
+ }, [ref, cursor, inputValue]);
 
 	mermaid.initialize({ startOnLoad: true });
 
 	const handleInputChange = async (event: React.ChangeEvent<HTMLTextAreaElement>): Promise<void> => {
 		const text = event.target.value;
+    const cursorPosition = event.target.selectionStart;
+    setCursor(cursorPosition);
 		try {
 			const diagram = await mermaid.render('schema', text);
 			const svg = diagram.svg;
@@ -28,7 +37,7 @@ const SchemaEditor: React.FC<Props> = ({ features }) => {
 			<div style={{ width: '50vw', height: '100vh' }}>
 				<h4 className='text-center mb-4'>Generated Query</h4>
 				{/* <textarea style={{ height: '90vh' }} value={inputValue} onChange={handleInputChange} className='form-control' /> */}
-				<textarea style={{ height: '90vh' }} value={inputValue} onChange={(e) => handleInputChange(e)} className='form-control' />
+				<textarea ref={ref} style={{ height: '90vh' }} value={inputValue} onChange={(e) => handleInputChange(e)} className='form-control' />
 			</div>
 			<div className='justify-content-center align-items-center'>
 				<div dangerouslySetInnerHTML={{ __html: diagramHTML }} style={{ width: '50vw' }} />
